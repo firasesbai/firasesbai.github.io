c266fce96f0a9dd5f01c5ea09cfcdc4c2783eaea
firasesbai <firasesbai@gmail.com>
2023-08-22 20:41:10 +0200

    Improve service worker logging

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,22 @@
 // Only trigger if service workers are supported in browser.
 if ('serviceWorker' in navigator) {
-    // Wait until window is loaded before registering.
-    window.addEventListener('load', () => {
-      // Register the service worker with "/" as it's scope.
-      navigator.serviceWorker.register('/sw.js', { scope: '/' })
-        // Output success/failure of registration.
-        .then(() => console.log('Service Worker registered with scope'))
-        .catch(() => console.error('Service Worker registration failed'));
-    });
-  }
+  // Wait until window is loaded before registering.
+  window.addEventListener('load', () => {
+    // Register the service worker with "/" as its scope.
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(registration => {
+        console.debug('Service Worker registered with scope:', registration.scope);
+
+        registration.addEventListener('updatefound', () => {
+          console.debug('Service Worker update found');
+          const newWorker = registration.installing;
+          newWorker.addEventListener('statechange', () => {
+            console.debug('Service Worker state changed to:', newWorker.state);
+          });
+        });
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
