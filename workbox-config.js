b072f7f2dc0c8a2797c993098b64db4639a3b20e
firasesbai <firasesbai@users.noreply.github.com>
2023-08-24 20:49:52 +0000

    deploy: e22556e7b6dc4e55d1148218c252360c47e5ace0

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: '_site/',
 	globPatterns: [
-		'**/*.{html,png,jpg,PNG,xml,ico,svg,webmanifest,css,js,txt,json}'
+		'**/*.{png,jpg,PNG,xml,ico,svg,webmanifest,css,js,txt,json}'
 	],
 	swDest: '_site/sw.js',
 	ignoreURLParametersMatching: [
