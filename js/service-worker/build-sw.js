3b29884679365deacc8e8eef675adf8f702487cb
firasesbai <firasesbai@users.noreply.github.com>
2025-10-31 15:03:56 +0000

    deploy: 851e579aaceabd362e4bd84636a8b63a5290d289

--- js/service-worker/build-sw.js
+++ js/service-worker/build-sw.js
@@ -1,16 +1,1 @@
-const { generateSW } = require('workbox-build');
-const workboxConfig = require('./workbox-config');
-
-generateSW(workboxConfig).then(({ count, size, warnings }) => {
-  if (warnings.length > 0) {
-    console.warn(
-      'Warnings encountered while generating a service worker:',
-      warnings.join('\n')
-    );
-  }
-
-  console.log(`Generated service worker, which will precache ${count} files, totaling ${size} bytes.`);
-}).catch((error) => {
-  console.error('Service worker generation failed:', error);
-  process.exit(1);
-});
+const{generateSW:generateSW}=require("workbox-build"),workboxConfig=require("./workbox-config");generateSW(workboxConfig).then(({count:e,size:r,warnings:o})=>{o.length>0&&console.warn("Warnings encountered while generating a service worker:",o.join("\n")),console.log(`Generated service worker, which will precache ${e} files, totaling ${r} bytes.`)})["catch"](e=>{console.error("Service worker generation failed:",e),process.exit(1)});
